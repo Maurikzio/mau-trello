6ce679927299665984923e49e940c84cd24a4226
mauricio <m.morocho1196@gmail.com>
2021-12-20 00:02:43 +0200

    Added secondary components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { AddNewItem } from './components/AddNewItem';
+import { Card } from './components/Card';
+import { Column } from './components/Column';
 import { AppContainer } from './styles';
 
 export const App = () => {
   return (
     <AppContainer>
-      Columns will go here
+      <Column text="To Do">
+        <Card text="Generate app aeiou" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn TypeScript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Using static typing" />
+      </Column>
+      <AddNewItem
+        toggleButtonText="+ Add another list"
+        onAdd={(text) => console.log(text)}
+      />
     </AppContainer>
   )
 }
